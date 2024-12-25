cd2e4a95a0d74d8eb8bce55679731f11aaa33f1f
Octo_Skull <17036615+Oct0Skull@users.noreply.github.com>
2024-12-25 02:14:22 +0000

    actualizacion 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🛠️ Pruebas en barra lateral 🛠️")
+st.write("probando menu en la barra lateral con app's complementarias")
+
+
